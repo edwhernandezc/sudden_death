9b350510c6275bc976c349f38c62a318fdd0d9e3
Edwin Hernandez Cabrera <info@encefalo.co>
2019-07-12 18:06:32 -0500

    Ajuste de 3 y 4 punto

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Primer punto
+
 def firtslist(list):  # Primero declaramos una función condicional
     for element in list:
         if list.index(element) == 0:
@@ -7,3 +9,32 @@ def firtslist(list):  # Primero declaramos una función condicional
 numeros = [['a', 'b', 'c'], [1, 2, 3]]
 
 list(filter(firtslist, numeros))
+
+
+# Fin primer punto
+
+# Tercer punto y cuarto
+def carga_lista():
+    li = []
+    for x in range(5):
+        valor = int(input("Ingrese valor:"))
+        li.append(valor)
+    return li
+
+
+def retornar_mayormenor(li):
+    ma = li[0]
+    me = li[0]
+    for x in range(1, len(li)):
+        if li[x] > ma:
+            ma = li[x]
+        else:
+            if li[x] < me:
+                me = li[x]
+    return [ma, me]
+
+lista = carga_lista()
+rango = retornar_mayormenor(lista)
+print("Mayor elemento de la lista:", rango[0])
+print("Mayor elemento de la lista:", rango[1])
+# Fin tercer y cuarto punto
